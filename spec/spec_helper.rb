b8dbe28ef1a9aabe422b9d6801a752e5c850acda
Adrian Lee Jackson <adrian.jackson@mymedsandme.com>
2018-01-08 14:29:30 +0000

    update specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,11 @@
-require 'spec'
+#require 'spec'
 require 'mocha'
 require 'active_record'
-require 'xamin'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-Spec::Runner.configure do |config|
-  
-end
+require_relative '../xamin'
 
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 load(File.dirname(__FILE__) + '/schema.rb')
--- spec/xamin_spec.rb
+++ spec/xamin_spec.rb
@@ -2,8 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Xamin" do
   before(:each) do
-    Xamin::Diagram.any_instance.stubs(:load_environment).returns(true)
-    Xamin::Diagram.any_instance.stubs(:files).returns(["app/models/author.rb", "app/models/book.rb"])
+    allow_any_instance_of(Xamin::Diagram).to receive(:load_environment).and_return(true)
+    allow_any_instance_of(Xamin::Diagram).to receive(:files).and_return(["app/models/author.rb", "app/models/book.rb"])
     @diagram = Xamin::Diagram.new
     @xmi = @diagram.to_xmi
   end
