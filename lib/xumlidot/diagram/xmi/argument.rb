3fe8ec4175ddcd61ee127c6bbf45f6d79234f3bb
Adrian Lee Jackson <adrian@smartpension.co.uk>
2018-02-25 20:34:24 +0000

    Fixes some xml entity translations & adds exclude

--- lib/xumlidot/diagram/xmi/argument.rb
+++ lib/xumlidot/diagram/xmi/argument.rb
@@ -8,14 +8,22 @@ module Xumlidot
       module Argument
         include ::Xumlidot::Diagram::Xmi::ID
 
-
         def draw
-          xmi = "<ownedParameter kind=\"inout\" name=\"#{name}\" xmi:id=\"#{id}\" xmi:type=\"uml:Parameter\">"
-          xmi += "<defaultValue value=\"#{default}\" xmi:id=\"#{default_id}\" xmi:type=\"uml:LiteralString\"/>" if default
+          xmi = "<ownedParameter kind=\"inout\" name=\"#{name_to_xmi}\" xmi:id=\"#{id}\" xmi:type=\"uml:Parameter\">"
+          xmi += "<defaultValue value=\"#{default_to_xmi}\" xmi:id=\"#{default_id}\" xmi:type=\"uml:LiteralString\"/>" if default
           xmi += "</ownedParameter>"
         end
 
         private
+
+        def name_to_xmi
+          name.encode(:xml => :text) if name
+        end
+
+        def default_to_xmi
+          return default unless default.is_a?(String)
+          default.encode(:xml => :text)
+        end
 
         def default_id
           @_default_id ||= new_id
--- lib/xumlidot/directory_tree.rb
+++ lib/xumlidot/directory_tree.rb
@@ -3,14 +3,20 @@ require 'find'
 module Xumlidot
   # Recurse down a directory tree
   class DirectoryTree
-    def initialize(directories)
+    def initialize(directories, options)
       @directories = directories
+      @options = options
+      @excluded = Regexp.new(@options.exclude)
     end
 
     def find_all_rb_files(&block)
       @directories.each do |directory|
+
         Find.find(directory) do |path|
+          next if path =~ @exluded
+
           next unless path.end_with? '.rb'
+
           yield path if block_given?
         end
       end
